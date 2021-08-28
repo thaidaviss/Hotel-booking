cb7c58f3ef5a08a2ad36f225d8e82547c6e455f0
unknown <thaidaviss1999@gmail.com>
2021-08-28 09:33:53 +0700

    initial

--- booking-app/src/index.js
+++ booking-app/src/index.js
@@ -1,17 +1,23 @@
+import 'antd/dist/antd.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+import store  from './store';
+import i18n from './locales/translation/i18n';
+import { I18nextProvider } from 'react-i18next';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <I18nextProvider i18n={i18n}>
+     <Provider store={store}>
+        <App />
+    </Provider>
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ booking-app/src/layouts/FullLayout/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';
+import Footer from '../Footer';
+
+FullLayout.propTypes = {
+   exact: PropTypes.bool,
+   path: PropTypes.string,
+   component:PropTypes.func,
+};
+
+FullLayout.defaultProps = {
+  exact: false,
+  path: null,
+  component:null,
+};
+
+function FullLayout(props) {
+  const { exact, path, component: Component } = props;
+  return (
+    <Route exact={exact} path={path}>
+    
+        <Component/>
+    
+      <Footer/>
+    </Route>
+
+  );
+}
+
+export default FullLayout;
