c74407545f119ac8bb706994fdc587727d7c1742
unknown <thaidaviss1999@gmail.com>
2021-09-03 10:25:47 +0700

    v1

---
+++ booking-app/src/pages/user/RoomDetailPage/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function RoomDetailPage(props) {
+  const {RoomDetail} = props
+  return (
+    <div className="room-detail">
+      <div className="room-detail__banner">
+
+      </div>
+      <div className="room-detail__body">
+          <div className="room-detail__left">
+            <div className="room-detail__slider">
+
+            </div>
+            <div className="room-detail__info">
+
+            </div>
+            <div className="room-detail__review">
+
+            </div>
+          </div>
+          <div className="room-detail__right">
+            <div className="room-detail__booking">
+
+            </div>
+           
+          </div>
+          <div className="room-detail__same">
+            
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default RoomDetailPage;
